dc1d78c14da8c6fc3ef1cfb4ca695e39b9d51496
Rafael <rafael.sl@usp.br>
2020-04-21 19:16:39 -0300

    Corrgindo bug da parede ilusória do Player 1

--- Ball.java
+++ Ball.java
@@ -123,22 +123,21 @@ public class Ball {
 	*/	
 
 	public boolean checkCollision(Player player){
-		if(player.getId().equals("Player 1") &&  
-		  (player.getCx() + player.getWidth()/2.0 >= cx - width/2.0 &&
-		  (cy - 1 <= player.getCy() - player.getHeight()/2.0 || cy + 1 >= player.getCy() + player.getHeight()))){
-			return true;
-		}else if(player.getId().equals("Player 2") && 
-		  (player.getCx() - player.getWidth()/2.0 <= cx + width/2.0 &&
-		  (player.getHeight()/2.0 + player.getCy() - (cy + height) >= 0 && player.getHeight()/2.0 + player.getCy() - (cy + height) <= player.getHeight()/2.0 + player.getCy() ))){
-			return true;
+		if(player.getId().equals("Player 1")) {
+			if(player.getCx() + player.getWidth()/2.0 >= cx - width/2.0 &&
+		      (player.getHeight()/2.0 + player.getCy() - (cy + height) >= 0 && 
+		       player.getHeight()/2.0 + player.getCy() - (cy + height) <= player.getHeight()/2.0 + player.getCy())){
+				return true;
+			}
+		}else if(player.getId().equals("Player 2")) {
+			if(player.getCx() - player.getWidth()/2.0 <= cx + width/2.0 &&
+		       (player.getHeight()/2.0 + player.getCy() - (cy + height) >= 0 && 
+		        player.getHeight()/2.0 + player.getCy() - (cy + height) <= player.getHeight()/2.0 + player.getCy())){
+				return true;
+			}
 		}
 		return false;
 	}
-
-	/** player 2
-	if(x da bolinha é maior ou igual a x do player  E  y da bolinha está entre as pontas do player  )
-	
-	*/
 
 	/**
 		Método que devolve a coordenada x do centro do retângulo que representa a bola.
